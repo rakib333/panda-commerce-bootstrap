fcb31b00642280acacc6715c20d592825bc0ed89
rakib771996 <smrakib333@gmail.com>
2021-08-17 03:44:24 +0600

    js added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// sob gula h2 er color change kora holo
+const h2 = document.getElementsByTagName('h2');
+for (const color of h2) {
+    color.style.color = 'lightblue';
+}
+// background color change kora holo
+const bgColor = document.getElementById('bagpack').style.backgroundColor = 'tomato';
+const paddingBottom = document.getElementById('bagpack').style.paddingBottom = '50px';
+// border radious kora holo
+const borderRadius = document.getElementsByClassName('card');
+for (const br of borderRadius) {
+    br.style.borderRadius = '30px';
+}
+
+
